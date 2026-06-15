e6175e860be7e610f0316dfcce8aec8bbbc8de56
dibyacodes <dibyacodes@gmail.com>
2026-06-15 08:44:57 +0530

    feat : create the banner, added a background grid in global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="bg-dot-grid">
+      <div className="bg-tailwind">
         <header className="px-10 py-5">
           <Image
             className="dark:invert"
@@ -70,7 +70,7 @@ export default function Home() {
           </div>
         </main>
 
-        <div className="flex max-block-2.5w-screen flex-col justify-center items-center">
+        <div className="flex max-w-screen flex-col justify-center items-center">
           <div className="max-w-[50%] min-w-[50%] flex flex-col gap-5">
             <div className="flex flex-col gap-2">
               <small className="font-mono font-semibold text-sm text-blue-500">
@@ -100,73 +100,49 @@ export default function Home() {
         </div>
       </div>
 
-      <div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div></div>
-      </div>
-      {/*<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/primecraft.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <div className="flex flex-col items-center w-full">
+        <div className="bg-zinc-900 min-w-5xl border-3 rounded-lg border-zinc-800 py-4 px-5 flex flex-row justify-between items-center">
+          <div className="flex flex-col gap-4">
+            <div>
+              <span className="font-sans font-semibold text-blue-100">
+                Primecraft
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-3xl font-sans max-w-lg tracking-[-2] font-medium">
+                Everything you need to build an online empire, and more.
+              </h1>
+              <p className="font-sans text-md font-medium text-zinc-500">
+                Ship like a god with the ultimate boilerplate
+              </p>
+            </div>
+            <div className="pt-5">
+              <div className="flex flex-row gap-3">
+                <input
+                  type="email"
+                  className="bg-[#19191C] border-2 border-[#27272A] py-2 px-4 rounded-full outline-none max-w-xl"
+                  placeholder="Email"
+                />
+                <button className="bg-[#DBEAFE] py-2 items-center justify-center border-2 border-transparent text-[#1447E6] font-sans font-medium px-4 outline-none rounded-full cursor-pointer hover:bg-white">
+                  Join Wait-list
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-5">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              className="dark:invert animate-bounce"
+              src={"/primecraft.svg"}
+              alt="primecraft logo"
+              width={100}
+              height={100}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <h1 className="font-mono animate-pulse text-blue-600">
+              Coming Soon...
+            </h1>
+          </div>
         </div>
-      </main>
-    </div>*/}
+      </div>
     </div>
   );
 }
